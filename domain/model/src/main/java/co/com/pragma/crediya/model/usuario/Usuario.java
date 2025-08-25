57addc92d0478a4178e8ec1b42e63f27621cb032
vescobarb <dtic.dev22@uis.edu.co>
2025-08-24 21:39:45 -0500

    Configuraciones iniciales

---
+++ domain/model/src/main/java/co/com/pragma/crediya/model/usuario/Usuario.java
@@ -0,0 +1,28 @@
+package co.com.pragma.crediya.model.usuario;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Usuario {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String fechaNacimiento;
+    private String direccion;
+    private String telefono;
+    private String correoElectronico;
+    private BigDecimal salarioBase;
+    private String documentoIdentidad;
+    private BigDecimal idRol;
+
+}
---
+++ domain/model/src/main/java/co/com/pragma/crediya/model/usuario/gateways/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package co.com.pragma.crediya.model.usuario.gateways;
+
+import co.com.pragma.crediya.model.usuario.Usuario;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UsuarioRepository {
+
+    Mono<Usuario> saveUser(Usuario usuario);
+    Flux<Usuario> getAllUsers();
+    Mono<Usuario> getUserById(Long id);
+    Mono<Usuario> editUser(Usuario usuario);
+    Mono<Void> deleteUser(Long id);
+}
